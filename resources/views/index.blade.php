05dd2b33b69fa10df40118e8cfffb85493d33be2
Reyezando <reynardo.etantyo2@gmail.com>
2025-07-04 19:28:42 +0700

    add myCareer.blade.php and myProjects.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,94 @@
 @endsection
 
 @section('add-on')
-    <link rel="stylesheet" href="./css/MyProjects.css">
+    <link rel="stylesheet" href="./css/Home.css">
 @endsection
 
 @section('content')
-
+    <!-- Intro -->
+    <div class="intro">
+        <div class="intro__pp">
+            <div class="intro__pp__circle">
+                <img src="./img/Foto Selfie (Reynardo Etantyo).jpg" class="intro__pp__img">
+            </div>
+        </div>
+        <div class="intro__text"></div>
+    </div>
+    <!-- Body -->
+    <div class="eduTitle">
+        <div></div>
+        <div>
+            EDUCATIONAL BACKGROUND
+        </div>
+    </div>
+    <div class="edutl">
+        <div></div>
+        <div>
+            <div></div>
+            <div>2003 - 2004</div>
+            <div>
+                <div>PLAYGROUP: KB/TK NOTRE DAME</div>
+                <div>West Jakarta, Puri Indah</div>
+            </div>
+        </div>
+        <div>
+            <div></div>
+            <div>2004 - 2005</div>
+            <div>
+                <div>KINDERGARTEN A: KB/TK NOTRE DAME</div>
+                <div>West Jakarta, Puri Indah</div>
+            </div>
+        </div>
+        <div>
+            <div></div>
+            <div>2005 - 2006</div>
+            <div>
+                <div>KINDERGARTEN B: STELLA MARIS SCHOOL</div>
+                <div>South Tangerang, BSD Serpong</div>
+            </div>
+        </div>
+        <div>
+            <div></div>
+            <div>2006 - 2009</div>
+            <div>
+                <div>PRIMARY: SAINT JOHN’S CATHOLIC SCHOOL</div>
+                <div>South Tangerang, BSD Serpong</div>
+            </div>
+        </div>
+        <div>
+            <div></div>
+            <div>2009 - 2012</div>
+            <div>
+                <div>PRIMARY: JOHN PAUL’S SCHOOL</div>
+                <div>West Bekasi, Harapan Indah</div>
+            </div>
+        </div>
+        <div>
+            <div></div>
+            <div>2012 - 2015</div>
+            <div>
+                <div>JUNIOR HIGH: JOHN PAUL’S SCHOOL</div>
+                <div>West Bekasi, Harapan Indah</div>
+            </div>
+        </div>
+        <div>
+            <div></div>
+            <div>2015 - 2018</div>
+            <div>
+                <div>SENIOR HIGH: SAINT JOHN’S CATHOLIC SCHOOL</div>
+                <div>South Tangerang, BSD Serpong<br>
+                    Science Major</div>
+            </div>
+        </div>
+        <div>
+            <div></div>
+            <div>2018 - 2023</div>
+            <div>
+                <div>COLLEGE: UNIVERSITAS MULTIMEDIA NUSANTARA</div>
+                <div>South Tangerang, Gading Serpong<br>
+                    Engineering & Informatics Faculty -<br>
+                    Informatics Study Program</div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/myCareer.blade.php
+++ resources/views/myCareer.blade.php
@@ -4,6 +4,51 @@
     My Career
 @endsection
 
-@section('content')
+@section('add-on')
+    <link rel="stylesheet" href="./css/MyCareer.css">
+@endsection
 
+@section('content')
+    <div class="body">
+        <div>
+            <p>Work at John Paul's School (2025-2030):</p>
+            <div id="slider-myCareer" class="splide" role="group" aria-label="Splide Basic HTML Example">
+                <div class="splide__track">
+                    <div class="splide__arrows">
+                        <button class="splide__arrow splide__arrow--prev">
+                            <img src="img/arrowLeft.png" alt="">
+                        </button>
+                        <button class="splide__arrow splide__arrow--next">
+                            <img src="img/arrowRight.png" alt="">
+                        </button>
+                    </div>
+                    <ul class="splide__list">
+                        <li class="splide__slide"><div class="imageWrap" style="background-image: url('img/undraw_professor_xcrw.png');"></div></li>
+                        <li class="splide__slide"><div class="imageWrap" style="background-image: url('img/undraw_mathematics_hc2c.png');"></div></li>
+                    </ul>
+                </div>
+            </div>
+            <p>2025-2030</p>
+        </div>
+        <div>
+            <p>Work at Ascendeum Remote Company (2026-2030):</p>
+            <div id="slider-myCareer" class="splide" role="group" aria-label="Splide Basic HTML Example">
+                <div class="splide__track">
+                    <div class="splide__arrows">
+                        <button class="splide__arrow splide__arrow--prev">
+                            <img src="img/arrowLeft.png" alt="">
+                        </button>
+                        <button class="splide__arrow splide__arrow--next">
+                            <img src="img/arrowRight.png" alt="">
+                        </button>
+                    </div>
+                    <ul class="splide__list">
+                        <li class="splide__slide"><div class="imageWrap" style="background-image: url('img/undraw_mathematics_hc2c.png');"></div></li>
+                        <li class="splide__slide"><div class="imageWrap" style="background-image: url('img/undraw_professor_xcrw.png');"></div></li>
+                    </ul>
+                </div>
+            </div>
+            <p>2026-2030</p>
+        </div>
+    </div>
 @endsection
